3d5d16a824d0ce3c8a8e66cb102c38fc6659f83d
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-09 20:03:33 -0300

    feat(routes): add new route (baseurl/new-product), to handle w. new products

---
+++ Front-End/src/pages/new-product/NewProduct.tsx
@@ -0,0 +1,9 @@
+const NewProduct = () => {
+	return (
+		<div>
+			New product page
+		</div>
+	);
+}
+
+export { NewProduct };
--- Front-End/src/routes/Routing.tsx
+++ Front-End/src/routes/Routing.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home";
 import { Error } from "../pages/error";
 import { LandingPage } from "../pages/landing-page";
+import { NewProduct } from "../pages/new-product/NewProduct";
 
 const Routing = (): React.ReactElement => {
     return(
@@ -9,6 +10,7 @@ const Routing = (): React.ReactElement => {
             <Routes>
                 <Route path="/" element={ <Home />}/>
                 <Route path="/explore" element={ <LandingPage />}/>
+                <Route path="/new-product" element={ <NewProduct />}/>
                 <Route path="*" element={ <Error />}/>
             </Routes>
         </BrowserRouter>
