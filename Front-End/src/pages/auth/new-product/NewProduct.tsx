c2c5d4b93d746fbf232e8016b36a878c44cdbc12
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-10 12:12:11 -0300

    feat(auth): add private route component and auth service instance, to handle w. authentication

--- Front-End/src/pages/auth/new-product/NewProduct.tsx
+++ Front-End/src/pages/auth/new-product/NewProduct.tsx
@@ -1,5 +1,6 @@
-import { useLayoutEffect, useState } from "react";
 import { gsap } from "gsap";
+import { useLayoutEffect, useState } from "react";
+import { PrivateRoute } from "../privete-route";
 
 import {
 	Label,
@@ -77,120 +78,122 @@ const NewProduct = (): React.ReactElement => {
 	}, []);
 	
 	return (
-		<Container id="container">
-			<TextContainer>
-				<Title>Criar um produto</Title>
-				<Subtitle>
-					Crie um novo produto abaixo, campos obrigatórios são marcados com{" "}
-					<span>*</span>
-				</Subtitle>
-			</TextContainer>
-			
-			<form onSubmit={ handleSubmit }>
-				<InputsFlex>
-					<InputContainer>
-						<Label htmlFor="name">Nome: <span>*</span></Label>
-						<InputFilled
-							type="text"
-							id="name"
-							required
-							value={name}
-							onChange={(e) => setName(e.target.value)}
-							placeholder="Ex.: Desodorante Antitranspirante Aerosol Masculino" />
-					</InputContainer>
-
-					<InputContainer>
-						<Label htmlFor="name">Marca: <span>*</span></Label>
-						<InputFilled
-							type="text"
-							id="name"
-							required
-							value={brand}
-							onChange={(e) => setBrand(e.target.value)}
-							placeholder="Ex.: Rexona" />
-					</InputContainer>
-
-					<InputContainer>
-						<Label htmlFor="description">Descrição: <span>*</span></Label>
-						<TextArea
-							id="description"
-							required
-							value={description}
-							onChange={(e) => setDescription(e.target.value)}
-							placeholder="Ex.: Desodorante Antitranspirante Aerosol Masculino Rexona Extracool 72 horas 150ml" />
-					</InputContainer>
-
-					<PriceContainer>
+		<PrivateRoute>
+			<Container id="container">
+				<TextContainer>
+					<Title>Criar um produto</Title>
+					<Subtitle>
+						Crie um novo produto abaixo, campos obrigatórios são marcados com{" "}
+						<span>*</span>
+					</Subtitle>
+				</TextContainer>
+				
+				<form onSubmit={ handleSubmit }>
+					<InputsFlex>
 						<InputContainer>
-							<Label htmlFor="price">
-								Preço: <span>*</span>
+							<Label htmlFor="name">Nome: <span>*</span></Label>
+							<InputFilled
+								type="text"
+								id="name"
+								required
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+								placeholder="Ex.: Desodorante Antitranspirante Aerosol Masculino" />
+						</InputContainer>
+
+						<InputContainer>
+							<Label htmlFor="name">Marca: <span>*</span></Label>
+							<InputFilled
+								type="text"
+								id="name"
+								required
+								value={brand}
+								onChange={(e) => setBrand(e.target.value)}
+								placeholder="Ex.: Rexona" />
+						</InputContainer>
+
+						<InputContainer>
+							<Label htmlFor="description">Descrição: <span>*</span></Label>
+							<TextArea
+								id="description"
+								required
+								value={description}
+								onChange={(e) => setDescription(e.target.value)}
+								placeholder="Ex.: Desodorante Antitranspirante Aerosol Masculino Rexona Extracool 72 horas 150ml" />
+						</InputContainer>
+
+						<PriceContainer>
+							<InputContainer>
+								<Label htmlFor="price">
+									Preço: <span>*</span>
+									<InputFilled
+										className="sm"
+										type="number"
+										id="price"
+										required
+										value={price}
+										onChange={(e) => setPrice(Number(e.target.value))}
+										placeholder="Ex.: 129.99" />
+								</Label>
+							</InputContainer>
+							<InputContainer>
+								<Label htmlFor="promotionalPrice">Preço promocional: <span className="text-muted">(Em caso de promoção)</span></Label>
 								<InputFilled
 									className="sm"
 									type="number"
-									id="price"
-									required
-									value={price}
-									onChange={(e) => setPrice(Number(e.target.value))}
-									placeholder="Ex.: 129.99" />
-							</Label>
+									id="promotionalPrice"
+									value={promotionalPrice}
+									onChange={(e) => setPromotionalPrice(Number(e.target.value))}
+									placeholder="Ex.: 129.99"/>
+							</InputContainer>
+						</PriceContainer>
+
+						<PharmaciesContainer>
+							<Label>Selecione a(s) farmácia(s): <span>*</span></Label>
+							<CheckBoxContainer>
+								<div>
+									<Label htmlFor="farmacia-01">Farmácia 01</Label>
+									<input id="farmacia-01" value="1" type="checkbox" />
+								</div>
+
+								<div>
+									<Label htmlFor="farmacia-02">Farmácia 02</Label>
+									<input id="farmacia-02" value="2" type="checkbox" />
+								</div>
+
+								<div>
+									<Label htmlFor="farmacia-03">Farmácia 03</Label>
+									<input id="farmacia-03" value="3" type="checkbox" />
+								</div>
+							</CheckBoxContainer>
+						</PharmaciesContainer>
+
+						<RadioContainer>
+							<Label htmlFor="prescriptionIsRequired">O produto necessita de receita? Clique aqui caso aplicavel</Label>
+							<input
+								type="checkbox"
+								id="prescriptionIsRequired"
+								value={prescriptionIsRequired} 
+								onChange={() => {prescriptionIsRequired === 1 ? setPrescriptionIsRequired(0) : setPrescriptionIsRequired(1)}} />
+						</RadioContainer>
+
+						<InputContainer className="file">
+							<Label>Selecine a imagem do produto: <span>*</span></Label>
+							<input
+								type="file"
+								required
+								accept="image/*"
+								onChange={handleFileChange} />
 						</InputContainer>
-						<InputContainer>
-							<Label htmlFor="promotionalPrice">Preço promocional: <span className="text-muted">(Em caso de promoção)</span></Label>
-							<InputFilled
-								className="sm"
-								type="number"
-								id="promotionalPrice"
-								value={promotionalPrice}
-								onChange={(e) => setPromotionalPrice(Number(e.target.value))}
-								placeholder="Ex.: 129.99"/>
-						</InputContainer>
-					</PriceContainer>
+					</InputsFlex>
 
-					<PharmaciesContainer>
-						<Label>Selecione a(s) farmácia(s): <span>*</span></Label>
-						<CheckBoxContainer>
-							<div>
-								<Label htmlFor="farmacia-01">Farmácia 01</Label>
-								<input id="farmacia-01" value="1" type="checkbox" />
-							</div>
-
-							<div>
-								<Label htmlFor="farmacia-02">Farmácia 02</Label>
-								<input id="farmacia-02" value="2" type="checkbox" />
-							</div>
-
-							<div>
-								<Label htmlFor="farmacia-03">Farmácia 03</Label>
-								<input id="farmacia-03" value="3" type="checkbox" />
-							</div>
-						</CheckBoxContainer>
-					</PharmaciesContainer>
-
-					<RadioContainer>
-						<Label htmlFor="prescriptionIsRequired">O produto necessita de receita? Clique aqui caso aplicavel</Label>
-						<input
-							type="checkbox"
-							id="prescriptionIsRequired"
-							value={prescriptionIsRequired} 
-							onChange={() => {prescriptionIsRequired === 1 ? setPrescriptionIsRequired(0) : setPrescriptionIsRequired(1)}} />
-					</RadioContainer>
-
-					<InputContainer className="file">
-						<Label>Selecine a imagem do produto: <span>*</span></Label>
-						<input
-							type="file"
-							required
-							accept="image/*"
-							onChange={handleFileChange} />
-					</InputContainer>
-				</InputsFlex>
-
-				<ButtonsContainer>
-					<Button className="save" type="submit">Salvar produto</Button>
-					<Button className="cancel">Cancelar</Button>
-				</ButtonsContainer>
-			</form>
-		</Container>
+					<ButtonsContainer>
+						<Button className="save" type="submit">Salvar produto</Button>
+						<Button className="cancel">Cancelar</Button>
+					</ButtonsContainer>
+				</form>
+			</Container>
+		</PrivateRoute>
 	);
 };
 
