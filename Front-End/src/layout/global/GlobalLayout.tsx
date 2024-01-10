e7575259a90e6392baf7876c8a9200af0910bf9b
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-10 16:16:56 -0300

    refactor(layout): add new global layout

---
+++ Front-End/src/layout/global/GlobalLayout.tsx
@@ -0,0 +1,23 @@
+import { HeaderAlternative } from "../../components/header/HeaderAlternative";
+import { WhatsappWidget } from "../../components/whatsapp-widget";
+import { GlobalContainer } from "../../pages/styles/global";
+import { AuthService } from "../../services/Auth";
+
+interface IGlobalLayoutProps {
+	children: React.ReactNode
+}
+
+const GlobalLayout = ({ children }: IGlobalLayoutProps) => {
+
+	const authService = new AuthService();
+
+	return (
+		<GlobalContainer>
+			<HeaderAlternative isLogged={ authService.isLogged() } />
+			{ children }
+			<WhatsappWidget />
+		</GlobalContainer>
+	);
+}
+
+export { GlobalLayout };
