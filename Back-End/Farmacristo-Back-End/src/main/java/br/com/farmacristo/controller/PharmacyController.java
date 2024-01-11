db8f28facf31bf4ca5601f3ff96e1159d8e08bb7
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-11 12:09:38 -0300

    feat(pharmacy): add new put method, to handle w. pharmacy update

--- Back-End/Farmacristo-Back-End/src/main/java/br/com/farmacristo/controller/PharmacyController.java
+++ Back-End/Farmacristo-Back-End/src/main/java/br/com/farmacristo/controller/PharmacyController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class PharmacyController {
 	}
 	
 	@GetMapping("/name-like/{name}")
-	public ResponseEntity<List<Pharmacy>> findByNameLike(@PathVariable(name="name") String name) throws FarmaCristoException {
+	public ResponseEntity<List<PharmacyDTO>> findByNameLike(@PathVariable(name="name") String name) throws FarmaCristoException {
 		return ResponseEntity.ok().body(this.pharmacyService.findByNameLike(name));
 	}
 	
@@ -50,7 +51,11 @@ public class PharmacyController {
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 	
-	
+	@PutMapping("/{pharmacyId}")
+	public ResponseEntity<Void> update(@PathVariable(name="pharmacyId") UUID id, @RequestBody NewPharmacyDTO updatedPharmacy) throws FarmaCristoException {
+		this.pharmacyService.updatePharmacy(id, updatedPharmacy);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@DeleteMapping("/{pharmacyId}")
 	public ResponseEntity<List<Pharmacy>> delete(@PathVariable(name="pharmacyId") UUID id) throws FarmaCristoException {
