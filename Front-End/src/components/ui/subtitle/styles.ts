60edd0caa122ce299507c15182a78e11715f8d0a
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-12 16:41:28 -0300

    feat(ui): add new ui components

--- Front-End/src/components/ui/subtitle/styles.ts
+++ Front-End/src/components/ui/subtitle/styles.ts
@@ -2,9 +2,7 @@ import styled from "styled-components";
 
 const Subtitle = styled.h2`
 	font-size: 0.75rem; /* 12px */
-	position: relative;
-	left: 0.25rem; /* 4px */
-
+	
 	span {
 		color: red;
 	}
