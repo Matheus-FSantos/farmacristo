fee5c60159b5ce41ea03be244bbf4e7fae1816fc
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-01-12 16:58:17 -0300

    refactor(ui): refactor on pages (add new component ui)

--- Front-End/src/pages/auth/signin/SignIn.tsx
+++ Front-End/src/pages/auth/signin/SignIn.tsx
@@ -8,7 +8,9 @@ import { Input } from "../../../components/ui/input/Input";
 import { Label } from "../../../components/ui/label/Label";
 import { Button } from "../../../components/ui/button/Button";
 import { Subtitle } from "../../../components/ui/subtitle/Subtitle";
+import { InputsFlex } from "../../../components/ui/containers/inputs-flex/InputsFlex";
 import { InputContainer } from "../../../components/ui/containers/input-container/InputContainer";
+import { TitleContainer } from "../../../components/ui/containers/title-container/TitleContainer";
 
 import styled from "styled-components";
 
@@ -16,18 +18,15 @@ const Container = styled.section`
 	padding: 2.5rem 0; /* 40px and 0 */
 `;
 
-const TitleContainer = styled.section`
-	display: flex;
-	flex-direction: column;
-	gap: 5px;
-`;
-
 const SignIn = (): React.ReactElement => {
-	
 	useDinamicTitle("Entre em sua conta");
-	
+
 	const [email, setEmail] = useState<string>("");
 	const [password, setPassword] = useState<string>("");
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();		
+	}
 
 	return (
 		<GlobalLayout>
@@ -37,34 +36,44 @@ const SignIn = (): React.ReactElement => {
 					<Subtitle>Realize sign-in com sua conta.</Subtitle>
 				</TitleContainer>
 
-				<InputContainer>
-					<Label For="email" Required={ true }>E-Mail:</Label>
-					<Input
-						Id="email"
-						Type="email"
-						Value={ email }
-						Required={ true }
-						SetValue={ (e) => setEmail(e.target.value) }
-						Placeholder="Ex.: exemplo@exemplo.com"
-					/>
-				</InputContainer>
+				<form onSubmit={handleSubmit}>
+					<InputsFlex>
+						<InputContainer>
+							<Label For="email" Required={true}>
+								E-Mail:
+							</Label>
+							<Input
+								Id="email"
+								Type="email"
+								Value={email}
+								Required={true}
+								SetValue={(e) => setEmail(e.target.value)}
+								Placeholder="Ex.: exemplo@exemplo.com"
+							/>
+						</InputContainer>
 
-				<InputContainer>
-					<Label For="password" Required={ true }>Senha:</Label>
-					<Input
-						Id="password"
-						Type="password"
-						Value={ password }
-						Required={ true }
-						SetValue={ (e) => setPassword(e.target.value) }
-						Placeholder="Ex.: YourPassword1234#"
-					/>
-				</InputContainer>
+						<InputContainer>
+							<Label For="password" Required={true}>
+								Senha:
+							</Label>
+							<Input
+								Id="password"
+								Type="password"
+								Value={password}
+								Required={true}
+								SetValue={(e) => setPassword(e.target.value)}
+								Placeholder="Ex.: YourPassword1234#"
+							/>
+						</InputContainer>
+					</InputsFlex>
 
-				<Button Type="submit" ButtonType="save">Entrar</Button>				
+					<Button Type="submit" ButtonType="save">
+						Entrar
+					</Button>
+				</form>
 			</Container>
 		</GlobalLayout>
 	);
-}
+};
 
 export { SignIn };
