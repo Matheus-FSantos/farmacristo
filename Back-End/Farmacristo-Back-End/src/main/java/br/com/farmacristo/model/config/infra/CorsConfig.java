010f9c5f1c414eaf6c326d5edbb8b5b8d223c816
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-02-08 13:14:01 -0300

    feat(cors): add cors config

---
+++ Back-End/Farmacristo-Back-End/src/main/java/br/com/farmacristo/model/config/infra/CorsConfig.java
@@ -0,0 +1,17 @@
+package br.com.farmacristo.model.config.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+	
+}
