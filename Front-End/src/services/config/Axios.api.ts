e77166aa60e7cb784c2a3ad919f1f2bfd3072996
Matheus-FSantos <matheus.fs.contato@gmail.com>
2024-05-30 18:13:28 -0300

    feat(render): add render on front-end

--- Front-End/src/services/config/Axios.api.ts
+++ Front-End/src/services/config/Axios.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_INSTANCE = axios.create({
-	baseURL: "http://localhost:8008/api",
+	baseURL: "https://farmacristo.onrender.com/api",
 });
 
 export { API_INSTANCE };
